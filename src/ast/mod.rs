6b446df3aff9473a55a978f6bfbf8d9aa56d7b41
YdrMaster <ydrml@hotmail.com>
2023-05-11 14:52:22 +0800

    feat: 为字面量支持 Algebra 转换
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -13,3 +13,15 @@ struct ExprParser;
 pub trait ToAlgebra {
     fn to_algebra(self) -> Algebra;
 }
+
+impl ToAlgebra for i64 {
+    fn to_algebra(self) -> Algebra {
+        unit::UnitExpr::Integer(self).to_algebra()
+    }
+}
+
+impl ToAlgebra for String {
+    fn to_algebra(self) -> Algebra {
+        unit::UnitExpr::Variable(self).to_algebra()
+    }
+}
--- src/ast/unit.rs
+++ src/ast/unit.rs
@@ -3,9 +3,9 @@ use pest::iterators::Pair;
 
 #[derive(Clone, Debug)]
 pub(super) enum UnitExpr {
-    Integer(Integer),
-    Variable(VarName),
-    Getter(Getter),
+    Integer(i64),
+    Variable(String),
+    Getter { list: String, idx: Algebra },
     Algebra(Algebra),
 }
 
@@ -15,89 +15,28 @@ impl From<Pair<'_, Rule>> for UnitExpr {
         let unit = pair.next().unwrap();
         assert_eq!(pair.next(), None);
         match unit.as_rule() {
-            Rule::integer => Self::Integer(unit.into()),
-            Rule::var_name => Self::Variable(unit.into()),
-            Rule::getter => Self::Getter(unit.into()),
+            Rule::integer => Self::Integer(unit.as_str().parse().unwrap()),
+            Rule::var_name => Self::Variable(unit.as_str().into()),
+            Rule::getter => {
+                let mut pair = unit.into_inner();
+                let array = pair.next().unwrap();
+                let index = pair.next().unwrap();
+                assert_eq!(pair.next(), None);
+                assert_eq!(array.as_rule(), Rule::list_name);
+                assert_eq!(index.as_rule(), Rule::algebra);
+                Self::Getter {
+                    list: array.as_str().to_string(),
+                    idx: index.into(),
+                }
+            }
             Rule::algebra => Self::Algebra(unit.into()),
             _ => unreachable!(),
         }
     }
 }
 
-impl From<Integer> for UnitExpr {
-    fn from(value: Integer) -> Self {
-        Self::Integer(value)
-    }
-}
-
-impl From<VarName> for UnitExpr {
-    fn from(value: VarName) -> Self {
-        Self::Variable(value)
-    }
-}
-
-impl From<Getter> for UnitExpr {
-    fn from(value: Getter) -> Self {
-        Self::Getter(value)
-    }
-}
-
-impl From<Algebra> for UnitExpr {
-    fn from(value: Algebra) -> Self {
-        Self::Algebra(value)
-    }
-}
-
 impl ToAlgebra for UnitExpr {
     fn to_algebra(self) -> super::Algebra {
         UnaryExpr::from(self).to_algebra()
-    }
-}
-
-#[derive(Clone, Copy, Default, Debug)]
-pub struct Integer(i64);
-
-impl From<Pair<'_, Rule>> for Integer {
-    fn from(value: Pair<'_, Rule>) -> Self {
-        Self(value.as_str().parse().unwrap())
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct VarName(String);
-
-impl From<Pair<'_, Rule>> for VarName {
-    fn from(value: Pair<'_, Rule>) -> Self {
-        Self(value.as_str().to_string())
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct ListName(String);
-
-impl From<Pair<'_, Rule>> for ListName {
-    fn from(value: Pair<'_, Rule>) -> Self {
-        Self(value.as_str().to_string())
-    }
-}
-
-#[derive(Clone, Debug)]
-pub(super) struct Getter {
-    array: ListName,
-    index: Algebra,
-}
-
-impl From<Pair<'_, Rule>> for Getter {
-    fn from(value: Pair<'_, Rule>) -> Self {
-        let mut pair = value.into_inner();
-        let array = pair.next().unwrap();
-        let index = pair.next().unwrap();
-        assert_eq!(pair.next(), None);
-        assert_eq!(array.as_rule(), Rule::list_name);
-        assert_eq!(index.as_rule(), Rule::algebra);
-        Self {
-            array: ListName(array.as_str().to_string()),
-            index: index.into(),
-        }
     }
 }
